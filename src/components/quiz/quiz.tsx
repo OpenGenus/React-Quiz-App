68f10511f9683fddfee033aa8fe5cf1f5360c29a
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-05-01 13:54:51 +0300

    Quiz question list style updated

--- src/components/quiz/quiz.tsx
+++ src/components/quiz/quiz.tsx
@@ -3,13 +3,30 @@ interface Question {
     question: string;
     answers: string[];
     correct: string;
+    Question: any;
 
     }
 const Quiz = (props: Question) =>{
-    console.log("Questions:",props)
+    console.log("Questions:",Object.keys(props).map((key)=>{
+        return props[key]
+    }))
     return(
         <div>
-            <h1>Ind Quiz</h1>
+           {Object.keys(props).map((key)=>{
+                return <div className="border flex flex-col p-7" key={key}>
+                    <div className="flex">
+                   <p className="text-2xl"> {props[key].question as any}</p>
+                   </div>
+                    <div className="flex flex-col  p-20">
+                        {props[key].answers.map((answer, i)=>{
+                            return <ul key={i}>
+                              <li><input type="radio" name="ans"/>
+                                {answer}</li>
+                            </ul>
+                        })}
+                </div>
+                </div>
+           })}
         </div>
     )
 }
