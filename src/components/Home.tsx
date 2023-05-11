4ad7553afccbdc83ba5a35e630a0d7a24d4d79dc
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-05-11 17:50:38 +0300

    Fixed issue on Home useeffect

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import { useEffect } from "react";
 import Quizs from "./quiz/Quizs";
 import { useState } from 'react'
 import { useNavigate } from "react-router-dom";
